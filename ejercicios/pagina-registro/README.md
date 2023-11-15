8dcff7614522802bb29a54efb703c76551936368
CrystinaG <crystinagonz@gmail.com>
2023-11-15 20:21:45 -0300

    Clase 6: Página de registro

---
+++ ejercicios/pagina-registro/README.md
@@ -0,0 +1,46 @@
+# Desarrollo Web con React + Vite + Bootstrap
+
+## Grupo Q - UTN - Argentina Programa 4.0
+
+Ejercicios utilizando las siguientes tecnologías y estructuras:
+
+- Proyecto creado con Vite
+- Archivo principal: `App.jsx`
+- Estilado con React-bootstrap, el cual tiene verdaderos componentes de React creados desde cero. La biblioteca es compatible con el núcleo de Bootstrap.
+  **Click en el Logo para ver componenetes**
+
+<p align="left"> <a href="https://react-bootstrap.netlify.app/docs/components/accordion" target="_blank" rel="noreferrer"> <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Ffreesvg.org%2Flogo-1674520932&psig=AOvVaw0c8O0SUEf5EcRQbBku1Oq2&ust=1700175321538000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIikpuOMx4IDFQAAAAAdAAAAABAE" alt="bootstrap" width="40" height="40"/> </a> </p>
+
+### Requisitos previos
+
+Antes de comenzar, asegurarse de tener instalado Node.js en la máquina. Para verificar esto ejecutar el siguiente comando en terminal:
+
+```
+    node --version
+```
+
+### Paso 1: Clonar el proyecto
+
+1.  Abrir la terminal y navegar hasta el directorio en el que desean clonar el proyecto:
+
+```
+    git clone https://github.com/CrysG011/EquipoQ.git
+```
+
+2.  Entrar al directorio del proyecto:
+
+```
+    cd <nombre-del-proyecto>
+```
+
+### Paso 2: Instalar todas las dependencias incluida React-bootstrap
+
+```
+    npm install
+```
+
+### Paso 3: Iniciar el servidor de desarrollo
+
+```
+    npm run dev
+```
---
+++ ejercicios/pagina-registro/src/components/RegistrationForm.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí la lógica para enviar los datos del formulario al servidor o hacer otras acciones necesarias.
+    console.log("Formulario enviado:", formData);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} className="fs-3 fw-bold text-center">
+      <Form.Group controlId="firstName">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Ingrese su nombre"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="lastName">
+        <Form.Label>Apellido</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Ingrese su apellido"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="email">
+        <Form.Label>Email</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Ingrese su email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="phone">
+        <Form.Label>Teléfono</Form.Label>
+        <Form.Control
+          type="tel"
+          placeholder="Ingrese su teléfono"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="password">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Ingrese su contraseña"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Form.Group controlId="confirmPassword">
+        <Form.Label>Confirmar Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Confirme su contraseña"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          required
+        />
+      </Form.Group>
+
+      <Button variant="dark" type="submit">
+        Registrarse
+      </Button>
+    </Form>
+  );
+};
+
+export default RegistrationForm;
