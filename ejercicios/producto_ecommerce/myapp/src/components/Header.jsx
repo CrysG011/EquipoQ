837eb72614fcbf1ebaa36b62c0308ed52db18dbb
CrystinaG <crystinagonz@gmail.com>
2023-11-24 16:29:30 -0300

    ajuste de img

--- ejercicios/producto_ecommerce/myapp/src/components/Header.jsx
+++ ejercicios/producto_ecommerce/myapp/src/components/Header.jsx
@@ -3,7 +3,9 @@ import { Filters } from "./Filters";
 export function Header() {
   return (
     <header>
-      <h1>Techno Shop</h1>
+      <h1 className="container text-center fw-bold text-success">
+        Techno Shop
+      </h1>
       <Filters />
     </header>
   );
